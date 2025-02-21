3fe8b25221d512111c6fe4e5bd1873fbd690b6f3
Max Esser <max@esser.codes>
2025-02-20 17:28:03 -0800

    using MakerSquirrel for Windows

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,11 +1,4 @@
 import {defineConfig} from "vite";
 
 // https://vitejs.dev/config
-export default defineConfig({
-    plugins: [],
-    build: {
-        rollupOptions: {
-            external: ["clipboardy"],
-        }
-    },
-});
+export default defineConfig({});
